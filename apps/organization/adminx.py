b452c9e39c5a65be6c06317c5dfb3c7d24f85289
erick <834424581@qq.com>
2018-02-22 22:15:09 +0800

    xadmin 添加数据表到后台显示

---
+++ apps/organization/adminx.py
@@ -0,0 +1,25 @@
+from .models import CourseOrg,City,Teacher
+import xadmin
+
+
+class CityAdmin():
+    list_display = ['desc', 'name', 'add_time']
+    search_fields = ['desc', 'name']
+    list_filter = ['desc', 'name', 'add_time']
+
+
+class CourseOrgAdmin():
+    list_display = ['name', 'desc','add_time','click_nums','fav_nums','image','address','city']
+    search_fields = ['name', 'desc','click_nums','fav_nums','image','address','city']
+    list_filter = ['name', 'desc','add_time','click_nums','fav_nums','image','address','city']
+
+
+class TeacherAdmin():
+    list_display = ['org', 'name', 'work_years','work_company','work_position','points','click_nums','fav_nums','add_time']
+    search_fields = ['org', 'name', 'work_years','work_company','work_position','points','click_nums','fav_nums']
+    list_filter = ['org', 'name', 'work_years','work_company','work_position','points','click_nums','fav_nums','add_time']
+
+
+xadmin.site.register(City,CityAdmin)
+xadmin.site.register(CourseOrg,CourseOrgAdmin)
+xadmin.site.register(Teacher,TeacherAdmin)

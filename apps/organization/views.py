7c50cd93431556d19bfe704e2ba73ec6848e783c
erick <834424581@qq.com>
2018-02-26 08:05:20 +0800

    机构数据分页展示利用django-pure-pagination

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .models import CourseOrg,City
+from pure_pagination import PageNotAnInteger,Paginator
 # Create your views here.
 
 
@@ -9,8 +10,17 @@ class OrgView(View):
         all_orgs = CourseOrg.objects.all()
         all_citys = City.objects.all()
         org_nums = all_orgs.count()
+        #对课程机构进行分页
+        try:
+            page_num = request.GET.get('page',1)
+        except PageNotAnInteger:
+            page_num = 1
+
+        p =Paginator(all_orgs,5,request=request)
+        orgs=p.page(page_num)
         return render(request,'org-list.html',{
-            "all_orgs":all_orgs,
+            "all_orgs":orgs,
             "all_citys":all_citys,
-            'org_nums':org_nums
+            'org_nums':org_nums,
+
         })

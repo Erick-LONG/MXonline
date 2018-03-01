1a50642c2b3c087decadd94d94169f9e2a578b73
erick <834424581@qq.com>
2018-03-01 23:02:08 +0800

    课程详情页推荐及收藏

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 # Create your views here.
 from .models import Course
+from operation.models import UserFavorite
 from pure_pagination import PageNotAnInteger,Paginator
 
 
@@ -35,4 +36,23 @@ class CourseDetailView(View):
         course = Course.objects.get(id = int(course_id))
         course.click_nums +=1
         course.save()
-        return render(request,'course-detail.html',{'course':course})
+
+        has_fav_course = False
+        has_fav_org = False
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user,fav_id=course.id,fav_type=1):
+                has_fav_course = True
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.course_org.id, fav_type=2):
+                has_fav_org = True
+
+        tag = course.tag
+        if tag:
+            relate_course = Course.objects.filter(tag=tag)[:1]
+        else:
+            relate_course=[]
+        return render(request,'course-detail.html',{
+            'course':course,
+            'relate_course':relate_course,
+            'has_fav_course':has_fav_course,
+            'has_fav_org':has_fav_org,
+        })
